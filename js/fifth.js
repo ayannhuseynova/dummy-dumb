dd22769dad6d010cd132f0a71c6c9f323eb2d6be
Ayan Huseynova <ayaneh@code.edu.az>
2022-11-01 17:49:28 +0400

    updated+added function

--- js/fifth.js
+++ js/fifth.js
@@ -1,21 +1,26 @@
 alert(
   "Write a JavaScript program to find the number of even digits in a an array of integers"
 );
-let count = prompt("how many numbers you want to enter?");
+
+let count = Number(prompt("how many numbers you want to enter?"));
+
 const newArr = new Array();
 const evenArr = new Array();
-for (let index = 0; index < count; index++) {
-  let newElement = Number(prompt("enter the number"));
-  newArr.push(newElement);
-}
-alert(`you created an array with elements {${newArr}}`);
-newArr.forEach((element) => {
-  if (element % 2 == 0 && element != 0) {
-    evenArr.push(element);
-  }
-});
-// alert(evenArr, evenArr.length);
+
+ConvertingToEven(newArr, evenArr, count);
+
 alert(
-  `your array was {${newArr}} with the length of {${newArr.length
-  }}. the new array is (${evenArr}), its' length is ${evenArr.length}`
+  `your array was {${newArr}} with the length of ${newArr.length}. the new array is (${evenArr}), its' length is ${evenArr.length}`
 );
+
+function ConvertingToEven(array1, array2, count) {
+  for (let index = 0; index < count; index++) {
+    let newElement = Number(prompt("enter the number"));
+    array1.push(newElement);
+  }
+  array1.forEach((element) => {
+    if (element % 2 == 0 && element != 0) {
+      array2.push(element);
+    }
+  });
+}
